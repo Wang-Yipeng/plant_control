1af66f67e2bbec7f16525b6f4939b9abb1a2c666
yipeng.wang <wangyp2@jihualab.com>
2021-10-12 19:11:58 +0800

    uart-PVS and SPD

--- untitled/serialRT.py
+++ untitled/serialRT.py
@@ -27,6 +27,7 @@ def CalculateBlockCRC32(ucBuffer):
 
     return ulCRC
 
+
 def SerialRx(mSerial):
 
     while(True):
@@ -41,39 +42,17 @@ def SerialRx(mSerial):
 
 def SerialTx(mSerial):
 
-    i=0
+    i = 0
     while(True):
-
-        HeadSB = 'AA 44 13'   # 同步数据头"AA 44 13 "
-        INSPVASB_HEAD_LEN = '58'
-        INSPVASB_HEAD_ID = 'FC 01'
-        INSPVASB_HEAD_GPSweek = '7C 08'
-
-        INSPVASB_HEAD_GPSminisec = '00 00 00 00'
-
-        INSPVASB_INF_GPSweek      = '7C 08 00 00'
-        INSPVASB_INF_GPSsec       = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_LATITUDE     = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_LONGTITUDE   = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_HEIGHT       = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_NORTH_VEL    = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_EAST_VEL     = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_UP_VEL       = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_ROLL         = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_PITCH        = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_AZIMUTH      = "00 00 00 00 00 00 00 00"
-        INSPVASB_INF_STATE        = "00 00 00 00"
-
-        INSPVASB_CRC32 = "00 00 00 00"
 
         HeadSB = bytearray.fromhex('AA 44 13')
         INSPVASB_HEAD_LEN = bytearray.fromhex('58')
         INSPVASB_HEAD_ID = bytearray.fromhex('FC 01')
         INSPVASB_HEAD_GPSweek = bytearray.fromhex('7C 08')
 
-        INSPVASB_HEAD_GPSminisec = struct.pack('<l', i)
+        INSPVASB_HEAD_GPSminisec = struct.pack('<L', i)
 
-        INSPVASB_INF_GPSweek = struct.pack('<l', 2048)
+        INSPVASB_INF_GPSweek = struct.pack('<L', 2048)
         INSPVASB_INF_GPSsec       = struct.pack('<d', i/10)
 
         INSPVASB_INF_LATITUDE     = struct.pack('<d',23.021726+i)
@@ -86,31 +65,58 @@ def SerialTx(mSerial):
 
         INSPVASB_INF_ROLL         = struct.pack('<d', 0.1)
         INSPVASB_INF_PITCH        = struct.pack('<d', 0.5)
-        INSPVASB_INF_AZIMUTH      = struct.pack('<d', math.asin(i/5000*math.pi))
+        INSPVASB_INF_AZIMUTH      = struct.pack('<d', i/10000)
 
         INSPVASB_INF_STATE        = struct.pack('<l', 2048)
 
+        INSPVASB = HeadSB+INSPVASB_HEAD_LEN + INSPVASB_HEAD_ID +\
+            INSPVASB_HEAD_GPSweek + INSPVASB_HEAD_GPSminisec +\
+            INSPVASB_INF_GPSweek + INSPVASB_INF_GPSsec +\
+            INSPVASB_INF_LATITUDE+INSPVASB_INF_LONGTITUDE+INSPVASB_INF_HEIGHT+\
+            INSPVASB_INF_NORTH_VEL+INSPVASB_INF_EAST_VEL+INSPVASB_INF_UP_VEL +\
+            INSPVASB_INF_ROLL+INSPVASB_INF_PITCH+INSPVASB_INF_AZIMUTH +\
+            INSPVASB_INF_STATE
 
-
-        INSPVASB = HeadSB+INSPVASB_HEAD_LEN+INSPVASB_HEAD_ID+INSPVASB_HEAD_GPSweek+INSPVASB_HEAD_GPSminisec+\
-        INSPVASB_INF_GPSweek+INSPVASB_INF_GPSsec+\
-        INSPVASB_INF_LATITUDE+INSPVASB_INF_LONGTITUDE+INSPVASB_INF_HEIGHT+\
-        INSPVASB_INF_NORTH_VEL+INSPVASB_INF_EAST_VEL+INSPVASB_INF_UP_VEL+\
-        INSPVASB_INF_ROLL+INSPVASB_INF_PITCH+INSPVASB_INF_AZIMUTH+\
-        INSPVASB_INF_STATE
-
-        INSPVASB_CRC32 = struct.pack('<L',CalculateBlockCRC32(INSPVASB))
-        INSPVASB=INSPVASB+INSPVASB_CRC32
+        INSPVASB_CRC32 = struct.pack(
+            '<L', CalculateBlockCRC32(INSPVASB))
+        INSPVASB = INSPVASB+INSPVASB_CRC32
 
         result = mSerial.write(INSPVASB)  # 写数据
-        print("写总字节数:", result)
-#        result2 = mSerial.write("abcde".encode("utf-8"))
-#        print("写总字节数:", result2)
+        print("写入INSPVASB，字节总数位为：", result)
+        time.sleep(0.5)
+
+        INSSPDSB_HEAD_LEN = bytearray.fromhex('28')
+        INSSPDSB_HEAD_ID = bytearray.fromhex('43 01')
+
+        INSSPDSB_HEAD_GPSweek = bytearray.fromhex('7C 08')
+        INSSPDSB_HEAD_GPSminisec = struct.pack('<L', i)
+
+        INSSPDSB_INF_GPSweek = struct.pack('<L', 2048)
+        INSSPDSB_INF_GPSsec = struct.pack('<d', i/10)
+
+        INSSPDSB_INF_Trk_gnd = struct.pack('<d', 0.1)
+        INSSPDSB_INF_Horizontal_Speed = struct.pack('<d', 0.5)
+        INSSPDSB_INF_Vertical_Speed = struct.pack(
+            '<d', math.asin(i/5000*math.pi))
+
+        INSSPDSB_INF_STATE = struct.pack('<l', 2048)
+
+        INSSPDSB = HeadSB+INSSPDSB_HEAD_LEN + INSSPDSB_HEAD_ID +\
+            INSSPDSB_HEAD_GPSweek + INSSPDSB_HEAD_GPSminisec +\
+            INSSPDSB_INF_GPSweek + INSSPDSB_INF_GPSsec +\
+            INSSPDSB_INF_Trk_gnd+INSSPDSB_INF_Horizontal_Speed +\
+            INSSPDSB_INF_Vertical_Speed +\
+            INSSPDSB_INF_STATE
+
+        INSSPDSB_CRC32 = struct.pack(
+            '<L', CalculateBlockCRC32(INSSPDSB))
+        INSSPDSB = INSSPDSB+INSSPDSB_CRC32
+
+        result2 = mSerial.write(INSSPDSB)  # 写数据
+        print("写入INSSPDSB，字节总数位为：", result2)
+
         i = i+1
         if i > 10000:
             i = 0
 
-        time.sleep(1)
-
-
-
+        time.sleep(0.5)
